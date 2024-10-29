a105b6335451a1d55b2bb3905822865d57f5f641
Daffa Alif Murtaja <daffa4lif@gmail.com>
2024-10-29 19:15:00 +0700

    add: costume colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,14 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        mainSky: "#C3EBFA",
+        mainSkyLight: "#EDF9FD",
+        mainPurple: "#CFCEFF",
+        mainPurpleLight: "#F1F0FF",
+        mainYellow: "#FAE27C",
+        mainYellowLight: "#FEFCE8"
+      }
     },
   },
   plugins: [],
