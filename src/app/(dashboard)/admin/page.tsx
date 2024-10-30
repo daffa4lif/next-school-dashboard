d7042743da607b0c09f5d1b9fc9b456804b378a2
Daffa Alif Murtaja <daffa4lif@gmail.com>
2024-10-30 10:45:05 +0700

    add: chart finance

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,6 +1,7 @@
 import UserCard from "@/components/admin/UserCard"
 import AttedanceChart from "@/components/charts/dashboard/AttedanceChart"
 import CountChart from "@/components/charts/dashboard/CountChart"
+import FinanceChart from "@/components/charts/dashboard/FinanceChart"
 
 const AdminPage = () => {
   return (
@@ -12,17 +13,19 @@ const AdminPage = () => {
           <UserCard type={"parent"} />
           <UserCard type={"staff"} />
         </div>
-        <div className="flex gap-4 flex-col lg:flex-row h-[450px]">
+        <div className="flex gap-4 flex-col lg:flex-row">
           {/* COUNT CHART */}
-          <div className="w-full lg:w-1/3">
+          <div className="w-full lg:w-1/3 h-[450px]">
             <CountChart />
           </div>
           {/* ATTAND CHART */}
-          <div className="w-full lg:w-2/3">
+          <div className="w-full lg:w-2/3 h-[450px]">
             <AttedanceChart />
           </div>
         </div>
-        <div className=""></div>
+        <div className="w-full h-[500px]">
+          <FinanceChart />
+        </div>
       </div>
       <div className="w-full lg:w-1/3">R</div>
     </div>
